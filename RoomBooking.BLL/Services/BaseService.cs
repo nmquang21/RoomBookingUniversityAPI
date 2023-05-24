b650088d27fe27e68d0bedfa82814fd4fd6b35e9
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-24 14:18:04 +0700

    Update BaseService.cs

--- RoomBooking.BLL/Services/BaseService.cs
+++ RoomBooking.BLL/Services/BaseService.cs
@@ -36,7 +36,46 @@ namespace RoomBooking.BLL.Services
         {
             _repository = repository;
         }
-
+        /// <summary>
+        /// Gửi email
+        /// </summary>
+        /// <param name="emailData"></param>
+        /// <returns></returns>
+        public string SendEmailString(EmailData emailData)
+        {
+            try
+            {
+                EmailSettings _emailSettings = new EmailSettings();
+                // Gán giá trị cho các thuộc tính của đối tượng
+                _emailSettings.EmailId = "bookingroomad@gmail.com";
+                _emailSettings.Name = "Support - Pro Code Guide";
+                _emailSettings.Password = "jeopyuiouqargvgs";
+                _emailSettings.Host = "smtp.gmail.com";
+                _emailSettings.Port = 465;
+                _emailSettings.UseSSL = true;
+                MimeMessage emailMessage = new MimeMessage();
+                MailboxAddress emailFrom = new MailboxAddress(_emailSettings.Name, _emailSettings.EmailId);
+                emailMessage.From.Add(emailFrom);
+                MailboxAddress emailTo = new MailboxAddress(emailData.EmailToName, emailData.EmailToId);
+                emailMessage.To.Add(emailTo);
+                emailMessage.Subject = emailData.EmailSubject;
+                BodyBuilder emailBodyBuilder = new BodyBuilder();
+                emailBodyBuilder.HtmlBody = emailData.EmailBody;
+                emailBodyBuilder.TextBody = emailData.EmailBody;
+                emailMessage.Body = emailBodyBuilder.ToMessageBody();
+                SmtpClient emailClient = new SmtpClient();
+                emailClient.Connect(_emailSettings.Host, _emailSettings.Port, _emailSettings.UseSSL);
+                emailClient.Authenticate(_emailSettings.EmailId, _emailSettings.Password);
+                emailClient.Send(emailMessage);
+                emailClient.Disconnect(true);
+                emailClient.Dispose();
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
         /// <summary>
         /// Thực hiện nghiệp vụ khi thêm mới người dùng
         /// </summary>
